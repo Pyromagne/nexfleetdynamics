a17fd6cd5c6102dd1f93aaac78197558fbcada5b
Pyromagne <growganoth826@gmail.com>
2025-05-07 13:34:24 +0800

    0705-homepage-wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
 import Logo from "./Components/Logo";
 import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Module from "./Components/Module";
+
+import { SiFacebook, SiInstagram, SiX } from "react-icons/si";
 
 const App = () => {
   return (
@@ -21,34 +22,57 @@ const App = () => {
               {nav.name}
             </NavLink>
           ))}
-          <Link to="/signup" className="text py-2 px-4 rounded-full bg-white text-black font-medium ml-2">Sign in</Link>
+          <Link to="/signup" className="py-2 px-4 rounded-full bg-white text-[#00446b] font-medium ml-2">Sign in</Link>
         </div>
       </nav>
 
       <main className="min-h-[34rem] mb-8">
-      <div className="px-20 py-16 text-white gradient2">
-        <h1 className="text-5xl noto-serif">A Complete Platform for Managing Modern Bus Transportation Systems</h1>
-        <h2 className="text-2xl noto-serif mt-10">Manage routes, bookings, staff, and live tracking — all from one easy-to-use dashboard.</h2>
-        
-        <div className="flex justify-center gap-8 text-black mt-20">
-          <Link to="/features" className="text-xl bg-white py-4 px-6 rounded-full font-medium">See Features</Link>
-          <Link to="/demo" className="text-xl bg-white py-4 px-6 rounded-full font-medium">Request a Demo</Link>
-        </div>
-      </div>
-        
+        <div className="px-20 py-16 text-white gradient2">
+          <h1 className="text-5xl noto-serif">A Complete Platform for Managing Modern Bus Transportation Systems</h1>
+          <h2 className="text-2xl noto-serif mt-10">Manage routes, bookings, staff, and live tracking — all from one easy-to-use dashboard.</h2>
 
-      <div className="mt-12 flex flex-col items-center">
-        <h3 className="italic text-center text-lg">Designed for transit authorities, bus companies, and fleet operators — our system simplifies operations, improves efficiency, and reduces costs.</h3>
-        
-        <h1 className="font-bold text-3xl mt-10 text-center">Modules</h1>
-        <div className="w-4/5 flex mt-6 flex-wrap gap-28 gap-y-12 justify-center">
-          {modules.map((module, index)=><Module key={index} name={module.name} desc={module.desc}/>)}
+          <div className="flex justify-center gap-8 text-black mt-20">
+            <Link to="/features" className="text-xl bg-white py-4 px-6 rounded-full font-medium text-[#00446b]">See Features</Link>
+            <Link to="/demo" className="text-xl text-white py-4 px-6 rounded-full font-medium gradient outline">Request a Demo</Link>
+          </div>
         </div>
-      </div>
+
+
+        <div className="my-12 flex flex-col items-center px-40">
+          <h3 className="italic text-center text-lg">Designed for bus transit companies — our system simplifies operations, improves efficiency, and reduces costs.</h3>
+
+          <h1 className="font-bold text-3xl mt-10 text-center">Modules</h1>
+          <div className="flex mt-6 flex-wrap gap-28 gap-y-12 justify-center">
+            {modules.map((module, index) => <Module key={index} name={module.name} desc={module.desc} />)}
+          </div>
+        </div>
       </main>
 
-      <footer className="h-48 mt-auto text-center p-4 border-t">
-        footer
+      <footer className="h-48 text-center p-4 border-t gradient flex flex-col text-white">
+        <div className="grid grid-cols-2">
+          <div className="w-72">
+            <Logo color="white"/>
+          </div>
+          <div>
+            <ul className="text-white text-right">
+              <li><a href="mailto:nexfleetdynamics@gmail.com">nexfleetdynamics@gmail.com</a></li>
+              <li><a href="sms:+639123456789?body=Please%20contact%20me.">0912 345 6789</a></li>
+              <li>Quezon City - Main Ofiice</li>
+            </ul>
+          </div>
+        </div>
+        <div className="border-t py-4 mt-auto flex">
+          <div className="flex gap-4 items-end">
+            <p className="mt-auto">© 2025 NexFleet Dynamics - All Rights Reserved.</p>
+            <Link className="hover:underline">Privacy Policy</Link>
+            <Link className="hover:underline">Term of Use</Link>
+          </div>
+          <div className="ml-auto flex gap-4">
+            <SiFacebook size={28} className="hover:cursor-pointer"/>
+            <SiInstagram size={28} className="hover:cursor-pointer"/>
+            <SiX size={28} className="hover:cursor-pointer"/>
+          </div>
+        </div>
       </footer>
     </div>
   );
@@ -80,7 +104,7 @@ const modules = [
 ];
 
 const navLinks = [
-  {   
+  {
     name: "Home",
     link: "/",
   },
--- src/Components/Module.jsx
+++ src/Components/Module.jsx
@@ -1,6 +1,6 @@
 const Module = ({ name, desc }) => {
   return (
-    <div className="rounded-2xl overflow-hidden w-64 shadow-2xl border-2 border-[#00446b]">
+    <div className="rounded-2xl overflow-hidden w-64 shadow-2xl border-1 border-[#00446b] hover:shadow-[#00446b] duration-200 cursor-default">
       <p className="bg-white p-6 text-center text-xl font-light">{name}</p>
       <p className="bg-[#00446b] p-4 text-white text-center text-sm italic h-32 font-light">{desc}</p>
     </div>
